fe88213efdc8adc3e42b3c97d4e6512dcad0d805
Momoko OSHIRO <e205430@ie.u-ryukyu.ac.jp>
2022-01-24 15:33:24 +0900

    1st commit

---
+++ report7.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+const int SIZE = 6;
+
+void sort(int array[SIZE], int size){
+    for (int k=0; k<SIZE; ++k) {
+        for (int l=k+1; l<SIZE; ++l) {
+            if (array[k] < array[l]) {
+                int tmp =  array[k];
+                array[k] = array[l];
+                array[l] = tmp;
+            }
+        }
+    }
+}
+int main(void){
+    int i,j;
+    const int SIZE = 6; //定数として配列のサイズを宣言                          
+    int scores[SIZE] = {100, 60, 70, 100, 90, 80};
+
+    printf("socores = ");
+    for (i=0; i<SIZE; ++i){
+        printf("%d ", scores[i]);
+    }
+    printf("\n");
+    
+
+    //処理コード
+    sort(scores,SIZE);
+
+    printf("results = ");
+    for (j=0; j<SIZE; ++j){
+        printf("%d ", scores[j]);
+    }
+    printf("\n");
+
+    return 0;
+}
